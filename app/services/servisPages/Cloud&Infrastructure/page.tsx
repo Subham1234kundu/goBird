537a33ecf94d14cc310fcea82bc71877104eb426
Sourav Mandal <souravmandal909p@gmail.com>
2026-03-07 13:16:45 +0530

    add

--- app/services/servisPages/Cloud&Infrastructure/page.tsx
+++ app/services/servisPages/Cloud&Infrastructure/page.tsx
@@ -379,7 +379,7 @@ const CloudInfrastructure = () => {
             className="cloud-infra-title text-white text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-[82px] 2xl:text-7xl font-light leading-tight mb-8 lg:mb-12 w-full sm:w-[90%]"
             style={{ fontWeight: 300 }}
           >
-            Secure Global Cloud and Data Infrastructure Businesses
+            Cloud, Data Residency & FinOps
           </h1>
           <h3 className="text-white font-light text-base sm:text-base md:text-md lg:text-md xl:text-[30px] 2xl:text-2xl w-full sm:w-[90%] md:w-[85%] lg:w-[75%] leading-[25px] sm:leading-10">
             Grobird designs, deploys, and operates cloud and data stacks with
